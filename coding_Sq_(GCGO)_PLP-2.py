1104ce47dcc5531e4e9c360694876ccc27f04efc
Nkennyelvis <e.nsengiman@alustudent.com>
2025-03-31 16:11:28 +0200

    Kenny's commit

--- coding_Sq_(GCGO)_PLP-2.py
+++ coding_Sq_(GCGO)_PLP-2.py
@@ -37,3 +37,27 @@ class QuizDatabase:
                 )
             """)
             conn.commit()
+
+    # Clear all quizzes from the database
+    def clear_database(self):
+        with self.connect() as conn:
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM quizzes")
+            conn.commit()
+
+    # Insert a new quiz into the database
+    def insert_quiz(self, subject, level, question, options, correct_answer, q_type):
+        with self.connect() as conn:
+            cursor = conn.cursor()
+            cursor.execute("""
+                INSERT INTO quizzes (subject, level, question, options, correct_answer, type)
+                VALUES (?, ?, ?, ?, ?, ?)
+            """, (subject, level, question, options, correct_answer, q_type))
+            conn.commit()
+
+    # Fetch quizzes from the database based on subject and level
+    def fetch_quizzes(self, subject, level):
+        with self.connect() as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM quizzes WHERE subject=? AND level=?", (subject, level))
+            return cursor.fetchall()
